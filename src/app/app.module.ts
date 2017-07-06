0826b4c64267fe639cfb01bd94b4f55e0c20c2e7
QNX-JESSICA-PC\QNX-JESSICA <victor.rafols>
2017-07-06 11:00:20 +0800

    Added navigation from home page to chat page by clicking the Title of the Home Page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ChatPage } from '../pages/chat/chat';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ChatPage } from '../../pages/chat/chat';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -11,4 +13,10 @@ export class HomePage {
 
   }
 
+  //V says: delete this, this is just for navigation to chat page
+  goToChatPage() {
+    this.navCtrl.push(ChatPage, {
+      test: 'test'
+    });
+  }
 }
