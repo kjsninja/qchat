5f736499e79b13b53d71ef3a3c69887fe21024f8
DESKTOP-KJHUE2T\SaitoMini <rafols.thor09@gmail.com>
2017-07-09 22:06:03 +0800

    navigator is removed.... click the L to got to chat...

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,9 +8,8 @@ import { ChatPage } from '../../pages/chat/chat';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
   constructor(public navCtrl: NavController) {
-
+    
   }
 
   //V says: delete this, this is just for navigation to chat page
